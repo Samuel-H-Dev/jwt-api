1fbab89e612fa94fbebaf19eada3cb099ce63d92
Samuel Holmes <samuelHdev@outlook.com>
2023-07-21 10:48:59 -0400

    added hashing to passwords

--- functions/src/users.js
+++ functions/src/users.js
@@ -1,22 +1,30 @@
 import { db } from "./dbConnect.js";
+import { hash } from "bcrypt";
 import jwt  from "jsonwebtoken";
 import { ObjectId } from "mongodb"
-import { secrets } from "../creds.js";
+import { secrets, salt } from "../creds.js";
 
 const coll = db.collection('users')
 
 export async function signup(req,res){
   const { email, password } = req.body
-  await coll.insertOne({ email: email.toLowerCase(), password }) //password stored as plain text (deal with this later)
+
+  const hashedPassword = await hash(password, salt) //hashing password
+  await coll.insertOne({ email: email.toLowerCase(), password: hashedPassword }) 
   login(req,res)
 }
 
 export async function login(req, res){
   const { email, password } = req.body
-  const user = await coll.findOne({ email: email.toLowerCase(), password })
+  const hashedPassword = await hash(password, salt) //hased for login 
+  const user = await coll.findOne({ email: email.toLowerCase(), password: hashedPassword }) //checks for hashed password 
+  if(!user){
+    res.status(401).send({ message: "invalid email or password" })
+    return
+  }
   delete user.password //strip out password 
-const token = jwt.sign(user, secrets)
-  res.send({ user, token })
+const token = jwt.sign(user, secrets)     //json web tokens
+  res.send({ user, token })     //json web tokens
 }
 
 
